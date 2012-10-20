1c53cc6c389d5ca2565b6f6a67dc4e431e3657af
cscscheng <cscscheng@gmail.com>
2012-10-20 22:24:05 +0800

    修改获取的数据..获取播放频道以及播放的节目名..

--- PPlive.py
+++ PPlive.py
@@ -21,35 +21,44 @@ def gen_pptv_list():
 				info=dict([('title',name),('url',fullurl)])
 				lists[id]=info
 				#last_time_dict[id]=0
-	print lists
+	#print lists
 	return lists
 
-##这里直接用replace替换掉反斜杠会更好..我不会用replace..哈哈..python现学.先用的.
-##可能有bug没仔细调试过..频道名称也没取出来..
 def gen_m3u8_url(in_url,id):
 	return_urls=[]
+	res={}
 	h=httplib2.Http()
 	resp,content=h.request(in_url,'GET')
 	if resp.status==200:
-		urls=re.findall('<[^<]+show_playing[^>]+>[^<]*<[^"]*".+\/([a-zA-Z0-9]*)\.html[^"]*".+>',content)
-		if urls:				
-			for url in urls:
-				resp,content=h.request('http://v.pptv.com/show/'+url+'.html','GET')
+		#print content
+		urls=re.findall('>(\\\u[^<]*)<[^>]*>[^<]*<[^>]*>[^<]*<[^<]+show_playing[^>]+>[^<]*<[^"]*"([^"]*)"[^>]+>[^<]*<[^>]*>[^<]*[^<]*<[^>]*>[^<]*[^<]*<[^>]*>[^<]*[^<]*<[^>]*>[^<]*[^<]*<[^>]*>([^<]*)<',content)
+		#urls=re.findall('<[^<]+show_playing[^>]+>[^<]*<[^"]*"([^"]*)"[^>]+>',content)
+		if urls:		
+			#print urls
+			#for url in urls:
+			for name,url,titles in urls:
+				js='["list", {"name":["%s","%s"]}]'%(name,titles)
+				j=json.loads(js)
+				#print j[1]['name'][0]
+				#print j[1]['name'][1]
+				url = url.replace('\\','')
+				res["tv_name"]=j[1]['name'][0]
+				res["playing"]=j[1]['name'][1]
+				resp,content=h.request(url,'GET')
 				if resp.status==200:
-					play_ids=re.findall('"ipadurl":"[^"]+web-m3u8-([0-9]+).m3u8"',content)
+					play_ids=re.findall('"ipadurl":"([^"]+)"',content)
 					if play_ids:
 						for play_id in play_ids:
-							m3u8url = 'http://web-play.pptv.com/web-m3u8-'+play_id+'.m3u8'
-							return_urls.append(m3u8url)
+							m3u8url = play_id.replace('\\','')
+							res["url"]=m3u8url
+							return_urls.append(res)
 	print return_urls
 	return return_urls
 				
 
 if __name__ == '__main__':
-	id='21'
+	id='25'
 	pptv_lists=gen_pptv_list()
 	gen_m3u8_url(pptv_lists[id]['url'],id)
-	
-	
-### omxplayer 直接支持m3u8的HLS流媒体播放..
+
 	
